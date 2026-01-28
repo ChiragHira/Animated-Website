d9e350e7d3889e582b03fefc13deb90e3c81c32a
ChiragHira <chiraghira39@gmail.com>
2026-01-28 17:03:16 +0530

    Footer is added

--- ochi/src/App.jsx
+++ ochi/src/App.jsx
@@ -2,6 +2,7 @@ import About from './components/About.jsx';
 import Cards from './components/Cards.jsx';
 import Eyes from './components/Eyes.jsx';
 import Feature from './components/Feature.jsx';
+import Footer from './components/Footer.jsx';
 import Landingpage from './components/Landingpage.jsx';
 import Marquee from './components/Marquee.jsx';
 import Navbar from './components/Navbar.jsx'
@@ -16,6 +17,7 @@ const App = () => {
       <Eyes />
       <Feature />
       <Cards />
+      <Footer />
     </div>
   )
 }
